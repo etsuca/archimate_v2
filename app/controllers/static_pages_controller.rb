b78bac31dbc87112d7600c9e059fa5c04f8e5e98
tsukasaebihara <ebiharatsukasa@gmail.com>
2026-04-23 09:31:45 +0900

    静的ページ追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,13 @@
 class StaticPagesController < ApplicationController
   def top
   end
+
+  def welcome
+  end
+
+  def terms
+  end
+
+  def privacy_policy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get "static_pages/top"
+  get "top", to: "static_pages#top"
+  get "welcome", to: "static_pages#welcome"
+  get "terms", to: "static_pages#terms"
+  get "privacy_policy", to: "static_pages#privacy_policy"
   get "up" => "rails/health#show", as: :rails_health_check
 
-  root 'static_pages#top'
+  root "static_pages#welcome"
 end
